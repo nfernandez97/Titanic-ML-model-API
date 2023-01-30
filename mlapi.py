9fde88c1e7ec55c285e901933dbf65e301a1d73e
Nicolas <nicofesan@hotmail.com>
2023-01-29 21:55:41 -0500

    V2.0

--- mlapi.py
+++ mlapi.py
@@ -6,7 +6,7 @@ from starlette.responses import RedirectResponse
 
 app = FastAPI()
 
-class Passanger(BaseModel):
+class Passanger(BaseModel): #Passanger class is created with its respective atributes
     Pclass: int #Int value (1,2,3)
     Sex: int # Int value 0=Female 1=Male
     Age: int # Int value
@@ -16,18 +16,18 @@ class Passanger(BaseModel):
     Embarked: int # Int value 0=Queenstown 1=Southampton 2=Cherbourg
 
 
-with open('model.pkl', 'rb') as f:
+with open('model.pkl', 'rb') as f: #Load the trained model on a .pkl format
     model = pickle.load(f)    
 
 @app.get('/')
 def root():
-    return RedirectResponse(url="/docs/")
+    return RedirectResponse(url="/docs/") #redirect the root path to the fastapi path
 
 @app.post('/predict')
-def prediction(item:Passanger):
-    df = pd.DataFrame([item.dict().values()],columns=item.dict().keys())
-    pred = model.predict(df)
-    if int(pred) == 1:
+def prediction(item:Passanger): #the function receives a passanger item
+    df = pd.DataFrame([item.dict().values()],columns=item.dict().keys()) #Create a dataframe with the information on the JSON request
+    pred = model.predict(df) #Predict 
+    if int(pred) == 1: #traslate the prediction into words
         pred = "Survived"
     else:
         pred = "Died"    
